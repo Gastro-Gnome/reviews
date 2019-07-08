a4950fdc155d636d1fca7bb24c62ab9d83cb0e28
Lee Graham <lbgraham@mac.com>
2019-07-08 10:11:58 -0700

    Initial commit with overall skeleton for reviews project.

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Successful request made to the reviews server.');
+});
+
+app.listen(port, () => {
+  console.log(`Reviews server is running on port ${port}`);
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+    entry: `${SRC_DIR}/index.jsx`,
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },    
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './public/dist')
+      },
+      devServer: {
+        contentBase: './dist'
+      }
+    };
